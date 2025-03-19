1ba99bec0bebb457e444964965a478b5a92dfa18
Anton Marek <antonlucasmarek@gmail.com>
2025-03-18 21:34:52 -0400

    changed player color to 'azure blue' holding keypress 'r' removes player momentarily

--- akgame.py
+++ akgame.py
@@ -3,18 +3,20 @@ import pygame
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREED_HEIGHT = 600
+SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREED_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 player = pygame.Rect((300, 250, 50, 50))
+
+pygame.display.set_caption("akgame V1.0.0")
 
 run = True
 while run:
 
-    screen.fill((0, 0, 0))
+    location = screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    pygame.draw.rect(screen, (0, 127, 255), player)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a] == True:
@@ -25,11 +27,15 @@ while run:
         player.move_ip(0, -1)
     elif key[pygame.K_s] == True:
         player.move_ip(0, 1)
+        #holding keypress 'r' removes player
+        invisible = pygame.key.get_pressed()
+    if key[pygame.K_r] == True:
+        location = screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
-    pygame.display.update()        
+
+    pygame.display.update()
 
 pygame.quit()
